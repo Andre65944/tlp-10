3255f250fee0798520bdc7d695f3c1fe4dd6274c
Andre65944 <tavaresandre464@gmail.com>
2024-12-20 16:26:00 +0000

    Add files via upload

---
+++ p.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int traduzir, letras = 0, i, frase[100], j=0;
+	char abc[5] = { 'a','e','i','o','u' };
+   cout << " quantas letras tem a palavra";
+   cin >> letras;
+   cout << "qual é a frase \n";
+   for (i = 0; i < letras; i++)
+   {
+	   cin >> frase[i];
+   }
+   for (i = 0; i < letras; i++)
+   {
+    if(frase[i]==abc[j])
+   }
+}
